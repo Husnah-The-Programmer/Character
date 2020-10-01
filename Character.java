16d60cf251f81be1cf886ccd57e39f3f00b8c576
Husnah-The-Programmer <67584836+Husnah-The-Programmer@users.noreply.github.com>
2020-10-01 13:01:51 +0100

    Add files via upload

---
+++ Character.java
@@ -0,0 +1,18 @@
+public class Character {
+    public static void main(String args[]) {
+        char ch1;
+        char ch2;
+        char ch3;
+        char ch4;
+
+        ch1 = 'a';
+        ch2 = 'l';
+        ch3 = '$';
+        ch4 = '+';
+
+        System.out.format("ch1: %c\n", ch1);
+        System.out.format("ch2: %c\n", ch2);
+        System.out.format("ch3: %c\n", ch3);
+        System.out.format("ch4: %c\n", ch4);
+    }
+}
